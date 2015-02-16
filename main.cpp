a6e73914505a20de30a61368f30434e096f2b5dc
Hubertzhang <hubert.zyk@gmail.com>
2015-02-16 20:53:38 +0800

    use QSettings instead of raw file operation, fix #6

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setOrganizationName("Lab µ");
+    QCoreApplication::setOrganizationDomain("lab.mu");
+    QCoreApplication::setApplicationName("tunet");
     QApplication a(argc, argv);
     LoginUi w;
     w.show();
